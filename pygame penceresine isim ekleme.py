eac4cb7482f1ec887837ae0ad6c54cb6449a9269
Melek Aylin <115497726+melekaylin@users.noreply.github.com>
2022-12-23 23:04:41 +0300

    Add files via upload

---
+++ pygame penceresine isim ekleme.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+
+pygame.init()
+
+pencere = pygame.display.set_mode((1000,700), pygame.SWSURFACE)
+pygame.display.set_caption("Işıklaar Ve Renkler")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.flip()
