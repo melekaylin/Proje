e54ebdd5a99c51a3613cd37d0d5170ceb23e2e23
NagihanAslan <115449049+NagihanAslan@users.noreply.github.com>
2022-12-24 22:56:08 +0300

    Create isinlar_sınıfı.py
    Ana renkteki ışınlar sınıfı

---
+++ isinlar_sınıfı.py
@@ -0,0 +1,24 @@
+class isinlar :
+
+    def kirmizi_isinlar ():
+        pygame.draw.aaline(ekran,kirmizi,(900,350),(100,150))
+        pygame.draw.aaline(ekran,kirmizi,(900,350),(100,250))
+        pygame.draw.aaline(ekran,kirmizi,(900,350),(100,350))
+        pygame.draw.aaline(ekran,kirmizi,(900,350),(100,450))
+        pygame.draw.aaline(ekran,kirmizi,(900,350),(100,550))
+
+
+    def yesil_isinlar ():
+        pygame.draw.aaline(ekran,yesil,(900,550),(100,0))
+        pygame.draw.aaline(ekran,yesil,(900,550),(100,100))
+        pygame.draw.aaline(ekran,yesil,(900,550),(100,200))
+        pygame.draw.aaline(ekran,yesil,(900,550),(100,300))
+        pygame.draw.aaline(ekran,yesil,(900,550),(100,400))
+
+
+    def mavi_isinlar ():
+        pygame.draw.aaline(ekran,mavi,(900,150),(100,300))
+        pygame.draw.aaline(ekran,mavi,(900,150),(100,400))
+        pygame.draw.aaline(ekran,mavi,(900,150),(100,500))
+        pygame.draw.aaline(ekran,mavi,(900,150),(100,600))
+        pygame.draw.aaline(ekran,mavi,(900,150),(100,700))
