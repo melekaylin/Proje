ffb0792567f6ebfbf2b773a375a0f0532a694869
NagihanAslan <115449049+NagihanAslan@users.noreply.github.com>
2022-12-24 10:43:02 +0300

    Update pygame penceresine isim ekleme.py
    yazım hatası düzeltildi.

--- pygame penceresine isim ekleme.py
+++ pygame penceresine isim ekleme.py
@@ -4,7 +4,7 @@ import sys
 pygame.init()
 
 pencere = pygame.display.set_mode((1000,700), pygame.SWSURFACE)
-pygame.display.set_caption("Işıklaar Ve Renkler")
+pygame.display.set_caption("Işık Ve Renkler")
 
 while True:
     for event in pygame.event.get():
